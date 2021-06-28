3a059783d033c0b5739dddef314454a0c2eb4c38
KimJeongguk <mindstar7949@naver.com>
2021-06-28 22:18:19 +0900

    Initial Commit

---
+++ Aqua_Talk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aqua_Talk
+//
+//  Created by Jeongguk Kim on 2021/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
