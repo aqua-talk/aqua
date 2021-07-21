7d4b685ae5f6e66743805868b5c7529fb3b0ec46
KimJeongguk <mindstar7949@naver.com>
2021-07-21 23:07:21 +0900

    UserInfoModel 작석중...

--- Aqua_Talk/UserInfoModel.swift
+++ Aqua_Talk/UserInfoModel.swift
@@ -6,17 +6,66 @@
 //
 
 import UIKit
+struct FriendInfo: Equatable {
+    let email: String
+    var name: String
+    var statusMessage: String?
+    var profile: UIImage?
+    
+    mutating func update(name: String, statusMessage: String, profile: UIImage){
+        self.name = name
+        self.statusMessage = statusMessage
+        self.profile = profile
+    }
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.email == rhs.email
+    }
+}
+class FriendsManager {
+    static let shared = FriendsManager()
+    
+    var friends: [FriendInfo] = []
+    
+    func createFriend(email: String, name: String, statusMessage: String, profile: UIImage) -> FriendInfo{
+        return FriendInfo(email: email, name: name, statusMessage: statusMessage, profile: profile)
+    }
+    func addFriend(_ friend: FriendInfo) {
+        friends.append(friend)
+        saveFriend()
+    }
+    func deleteFriend(_ friend: FriendInfo) {
+        friends = friends.filter { $0.email != friend.email }
+        saveFriend()
+    }
+    func updateFriend(_ friend: FriendInfo) {
+        guard let index = friends.firstIndex(of: friend) else {
+            return
+        }
+        friends[index].update(name: friend.name, statusMessage: friend.statusMessage ?? "", profile: friend.profile ?? UIImage())//여기 나중에 디폴트 이미지 구해서 ?? 뒤에 넣어줘야함
+        saveFriend()
+    }
+    func saveFriend(){
+        
+    }
+}
+
 
 struct UserInfo {
     let email: String
-    let name: String
-    let password: String
+    var name: String
+    var password: String
+    var statusMessage: String?
     var profile: UIImage?
+    var firends: [FriendInfo]?
     
-    init(email: String, name: String, password: String){
-        self.email = email
+    mutating func update(name: String, password: String, statusMessage: String, profile: UIImage, friends: [FriendInfo]){
         self.name = name
         self.password = password
+        self.statusMessage = statusMessage
+        self.profile = profile
+        self.firends = friends
     }
+    
 
 }
