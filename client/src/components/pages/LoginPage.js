e8c7d4dd98df9014f918d3c21c7d1f997219fa50
pancomplex <renaissance21c@gmail.com>
2021-08-20 18:20:15 +0900

    ajax test

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -1,10 +1,28 @@
 import React from "react";
+import axios from "axios";
 import { Button } from "react-bootstrap";
 
-function LoginPage() {
+function LoginPage(props) {
   return (
     <div>
-      <Button variant="primary" href="http://localhost:3002/auth/google">
+      {/* <Button variant="primary" href="http://localhost:3002/auth/google">
+        구글
+      </Button> */}
+
+      <Button
+        variant="primary"
+        onClick={() => {
+          axios
+            .get("/auth/google")
+            .then(function (response) {
+              console.log("response");
+              props.login(true);
+            })
+            .catch(function (error) {
+              console.log("실패");
+            });
+        }}
+      >
         구글
       </Button>
     </div>
--- client/src/components/pages/MainPage/FriendPanel/FriendHeader.js
+++ client/src/components/pages/MainPage/FriendPanel/FriendHeader.js
@@ -5,12 +5,12 @@ function FriendHeader() {
   return (
     <div style={{ paddingTop: 30, display: "flex", justifyContent: "space-between" }}>
       <h2 style={{ textAlign: "left", fontSize: "1.5rem", fontWeight: 600 }}>친구</h2>
-      <ul style={{ display: "flex" }}>
+      <ul style={{ display: "flex", gap: 15 }}>
         <li>
           <BiSearch size={25} />
         </li>
         <li>
-          <BiUserPlus size={25} style={{ marginLeft: 15 }} />
+          <BiUserPlus size={25} />
         </li>
       </ul>
     </div>
